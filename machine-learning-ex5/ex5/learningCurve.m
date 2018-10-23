b2dc30890785c3b2665f21aa8ea68e19eb534785
nobso <sureshkm@nobso.com>
2018-10-23 16:26:30 -0700

    completing Learning curves

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -54,11 +54,23 @@ error_val   = zeros(m, 1);
 % ---------------------- Sample Solution ----------------------
 
 
+for i = 1:m
 
+    % training and cross validation set error for different training set sizes
+    X_train = X(1:i, :);
+    y_train = y(1:i);
 
+    
+    theta = trainLinearReg(X_train, y_train, lambda);
 
+    % set λ to 0 only when using it to compute the training error and cross validation error
+    [J_train, grad_train] = linearRegCostFunction(X_train, y_train, theta, 0); 
+    [J_val, grad_val] = linearRegCostFunction(Xval, yval, theta, 0); 
 
+    error_train(i) = J_train;
+    error_val(i) = J_val;
 
+end
 % -------------------------------------------------------------
 
 % =========================================================================
