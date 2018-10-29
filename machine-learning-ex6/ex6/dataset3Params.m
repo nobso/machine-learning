03e57c1667e26099461da6592283eafe6c6c123e
nobso <sureshkm@nobso.com>
2018-10-29 14:59:45 -0700

    SVM › SVM with Gaussian Kernels

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 1; % optimal value after the training is 1.0
+sigma = 0.3; % optimal value after the training is 0.10
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,12 +23,40 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+multiplicativeValues = [ 0.01 0.03 0.1 0.3 1 3 10 30 ];
+multiplicativeValuesLength = length(multiplicativeValues);
+errorMin = inf;
+
+for i=1:multiplicativeValuesLength
+    
+    % regularized parameter
+    tempC = multiplicativeValues(i); 
+
+    for j=1:multiplicativeValuesLength
+        % Gaussin or similarity function Sigma
+        tempSigma = multiplicativeValues(j);
+
+        % Train the SVM
+        model= svmTrain(X, y, tempC, @(x1, x2) gaussianKernel(x1, x2, tempSigma));
+
+        % prediction using the trained model against the validation set
+        predictions = svmPredict(model, Xval);
+
+        % compute the error 
+        computedError = mean(double(predictions ~= yval));
+
+        % choose the least
+        if ( computedError <= errorMin )
+            C = tempC;
+            sigma = tempSigma;
+            errorMin = computedError;
+        end
+    end
+    fprintf('iteration, [C, sigma] = %f [%f %f]\n', i, C, sigma);
+end
 
 
 
-
-
-
-% =========================================================================
+    % =========================================================================
 
 end
