a9f11470425ad7cc8a8aee4a1eb94539ba673699
nobso <sureshkm@nobso.com>
2018-11-01 22:00:02 -0700

    K-Means › random initialization is updated

--- machine-learning-ex7/ex7/ex7.m
+++ machine-learning-ex7/ex7/ex7.m
@@ -123,7 +123,7 @@ X = reshape(A, img_size(1) * img_size(2), 3);
 
 % Run your K-Means algorithm on this data
 % You should try different values of K and max_iters here
-K = 16; 
+K = 5; 
 max_iters = 10;
 
 % When using K-Means, it is important the initialize the centroids
@@ -132,7 +132,7 @@ max_iters = 10;
 initial_centroids = kMeansInitCentroids(X, K);
 
 % Run K-Means
-[centroids, idx] = runkMeans(X, initial_centroids, max_iters);
+[centroids, idx] = runkMeans(X, initial_centroids, max_iters, true);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
